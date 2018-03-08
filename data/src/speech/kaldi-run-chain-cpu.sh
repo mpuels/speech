941d8e04d9d3761938dddb89b04309a8968f03ad
Marc Püls <mpuels@gmail.com>
2018-03-08 11:01:04 +0100

    refactor: Introduce stages to kaldi-run-chain-cpu.sh

--- data/src/speech/kaldi-run-chain-cpu.sh
+++ data/src/speech/kaldi-run-chain-cpu.sh
@@ -61,140 +61,198 @@ fi
 
 echo "Runtime configuration is: nJobs $nJobs, nDecodeJobs $nDecodeJobs. If this is not what you want, edit cmd.sh"
 
-# now start preprocessing with KALDI scripts
+if [ $stage -le 0 ]; then
+    # now start preprocessing with KALDI scripts
 
-# remove old lang dir if it exists
-rm -rf data/lang
+    # remove old lang dir if it exists
+    rm -rf data/lang
 
-# Prepare phoneme data for Kaldi
-utils/prepare_lang.sh data/local/dict $(get_oov_symbol) data/local/lang data/lang
+    # Prepare phoneme data for Kaldi
+    utils/prepare_lang.sh data/local/dict $(get_oov_symbol) data/local/lang data/lang
+fi
 
 #
 # make mfcc
 #
-make_mfcc_begin_utc=$(now_utc)
-for datadir in train test; do
-    utils/fix_data_dir.sh data/$datadir 
-    steps/make_mfcc.sh --cmd "$train_cmd" --nj $nJobs data/$datadir exp/make_mfcc_chain/$datadir $mfccdir || exit 1;
-    utils/fix_data_dir.sh data/${datadir} # some files fail to get mfcc for many reasons
-    steps/compute_cmvn_stats.sh data/${datadir} exp/make_mfcc_chain/$datadir $mfccdir || exit 1;
-    utils/fix_data_dir.sh data/${datadir} # some files fail to get mfcc for many reasons
-done
-log_begin_end make_mfcc ${make_mfcc_begin_utc}
+if [ $stage -le 1 ]; then
+    make_mfcc_begin_utc=$(now_utc)
+    for datadir in train test; do
+        utils/fix_data_dir.sh data/$datadir
+        steps/make_mfcc.sh --cmd "$train_cmd" \
+                           --nj $nJobs \
+                           data/$datadir \
+                           exp/make_mfcc_chain/$datadir \
+                           $mfccdir \
+            || exit 1;
+        # Some files fail to get mfcc for many reasons.
+        utils/fix_data_dir.sh data/${datadir}
+        steps/compute_cmvn_stats.sh data/${datadir} \
+                                    exp/make_mfcc_chain/$datadir \
+                                    $mfccdir \
+            || exit 1;
+        # Some files fail to get mfcc for many reasons.
+        utils/fix_data_dir.sh data/${datadir}
+    done
+    log_begin_end make_mfcc ${make_mfcc_begin_utc}
+fi
 
-echo
-echo mono0a_chain
-echo
+if [ $stage -le 2 ]; then
+    echo
+    echo mono0a_chain
+    echo
 
-mono0a_chain_begin_utc=$(now_utc)
-steps/train_mono.sh --nj $nJobs --cmd "$train_cmd" \
-  data/train data/lang exp/mono0a_chain || exit 1;
-log_begin_end steps/train_mono.sh ${mono0a_chain_begin_utc}
+    mono0a_chain_begin_utc=$(now_utc)
+    steps/train_mono.sh --nj $nJobs --cmd "$train_cmd" \
+                        data/train data/lang exp/mono0a_chain || exit 1;
+    log_begin_end steps/train_mono.sh ${mono0a_chain_begin_utc}
+fi
 
-echo
-echo tri1_chain
-echo
+if [ $stage -le 3 ]; then
+    echo
+    echo tri1_chain
+    echo
 
-tri1_chain_align_si_begin_utc=$(now_utc)
-steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
-  data/train data/lang exp/mono0a_chain exp/mono0a_ali_chain || exit 1;
-log_begin_end "tri1:steps/align_si.sh" ${tri1_chain_align_si_begin_utc}
+    tri1_chain_align_si_begin_utc=$(now_utc)
+    steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
+                      data/train data/lang \
+                      exp/mono0a_chain \
+                      exp/mono0a_ali_chain \
+        || exit 1;
+    log_begin_end "tri1:steps/align_si.sh" ${tri1_chain_align_si_begin_utc}
+fi
 
-tri1_train_deltas_begin_utc=$(now_utc)
-steps/train_deltas.sh --cmd "$train_cmd" 2000 10000 \
-  data/train data/lang exp/mono0a_ali_chain exp/tri1_chain || exit 1;
-log_begin_end "tri1:steps/train_deltas.sh" ${tri1_train_deltas_begin_utc}
+if [ $stage -le 4 ]; then
+    tri1_train_deltas_begin_utc=$(now_utc)
+    steps/train_deltas.sh --cmd "$train_cmd" 2000 10000 \
+                          data/train \
+                          data/lang \
+                          exp/mono0a_ali_chain \
+                          exp/tri1_chain \
+        || exit 1;
+    log_begin_end "tri1:steps/train_deltas.sh" ${tri1_train_deltas_begin_utc}
+fi
 
-echo
-echo tri2b_chain
-echo
+if [ $stage -le 5 ]; then
+    echo
+    echo tri2b_chain
+    echo
 
-tri2_align_si_begin_utc=$(now_utc)
-steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
-  data/train data/lang exp/tri1_chain exp/tri1_ali_chain || exit 1;
-log_begin_end "tri2:steps/align_si.sh" ${tri2_align_si_begin_utc}
+    tri2_align_si_begin_utc=$(now_utc)
+    steps/align_si.sh --nj $nJobs --cmd "$train_cmd" \
+                      data/train \
+                      data/lang \
+                      exp/tri1_chain \
+                      exp/tri1_ali_chain \
+        || exit 1;
+    log_begin_end "tri2:steps/align_si.sh" ${tri2_align_si_begin_utc}
+fi
 
-tri2_train_lda_mllt_begin_utc=$(now_utc)
-steps/train_lda_mllt.sh --cmd "$train_cmd" \
-  --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
-  data/train data/lang exp/tri1_ali_chain exp/tri2b_chain || exit 1;
-log_begin_end "tri2:steps/train_lda_mllt.sh" ${tri2_train_lda_mllt_begin_utc}
+if [ $stage -le 6 ]; then
+    tri2_train_lda_mllt_begin_utc=$(now_utc)
+    steps/train_lda_mllt.sh --cmd "$train_cmd" \
+                            --splice-opts "--left-context=3 --right-context=3" \
+                            2500 15000 \
+                            data/train \
+                            data/lang \
+                            exp/tri1_ali_chain \
+                            exp/tri2b_chain \
+        || exit 1;
+    log_begin_end "tri2:steps/train_lda_mllt.sh" \
+                  ${tri2_train_lda_mllt_begin_utc}
+fi
 
-tri2_mkgraph_begin_utc=$(now_utc)
-utils/mkgraph.sh data/lang_test \
-  exp/tri2b_chain exp/tri2b_chain/graph || exit 1;
-log_begin_end "tri2:utils/mkgraph.sh" ${tri2_mkgraph_begin_utc}
+if [ $stage -le 7 ]; then
+    tri2_mkgraph_begin_utc=$(now_utc)
+    utils/mkgraph.sh data/lang_test \
+                     exp/tri2b_chain exp/tri2b_chain/graph || exit 1;
+    log_begin_end "tri2:utils/mkgraph.sh" ${tri2_mkgraph_begin_utc}
+fi
 
-echo
-echo run_ivector_common.sh
-echo
+if [ $stage -le 8 ]; then
+    echo
+    echo run_ivector_common.sh
+    echo
 
-run_ivector_common_begin_utc=$(now_utc)
-local/nnet3/run_ivector_common.sh --stage $stage \
-                                  --nj $nJobs \
-                                  --min-seg-len $min_seg_len \
-                                  --train-set $train_set \
-                                  --gmm $gmm \
-                                  --num-threads-ubm $num_threads_ubm \
-                                  --nnet3-affix "$nnet3_affix"
-log_begin_end local/nnet3/run_ivector_common.sh ${run_ivector_common_begin_utc}
-
-gmm_dir=exp/$gmm
-ali_dir=exp/${gmm}_ali_${train_set}_sp_comb
-lang=data/lang_chain
-lores_train_data_dir=data/${train_set}_sp_comb
+    run_ivector_common_begin_utc=$(now_utc)
+    local/nnet3/run_ivector_common.sh --stage $stage \
+                                      --nj $nJobs \
+                                      --min-seg-len $min_seg_len \
+                                      --train-set $train_set \
+                                      --gmm $gmm \
+                                      --num-threads-ubm $num_threads_ubm \
+                                      --nnet3-affix "$nnet3_affix"
+    log_begin_end local/nnet3/run_ivector_common.sh \
+                  ${run_ivector_common_begin_utc}
+fi
 
 for f in $gmm_dir/final.mdl $train_data_dir/feats.scp $train_ivector_dir/ivector_online.scp \
     $lores_train_data_dir/feats.scp $ali_dir/ali.1.gz; do
   [ ! -f $f ] && echo "$0: expected file $f to exist" && exit 1
 done
 
-echo
-echo creating lang directory with one state per phone.
-echo
+if [ $stage -le 9 ]; then
+    echo
+    echo creating lang directory with one state per phone.
+    echo
 
-if [ -d data/lang_chain ]; then
-  if [ data/lang_chain/L.fst -nt data/lang/L.fst ]; then
-    echo "$0: data/lang_chain already exists, not overwriting it; continuing"
-  else
-    echo "$0: data/lang_chain already exists and seems to be older than data/lang..."
-    echo " ... not sure what to do.  Exiting."
-    exit 1;
-  fi
-else
-  cp -r data/lang data/lang_chain
-  silphonelist=$(cat data/lang_chain/phones/silence.csl) || exit 1;
-  nonsilphonelist=$(cat data/lang_chain/phones/nonsilence.csl) || exit 1;
-  # Use our special topology... note that later on may have to tune this
-  # topology.
-  gen_topo_begin_utc=$(now_utc)
-  steps/nnet3/chain/gen_topo.py $nonsilphonelist $silphonelist >data/lang_chain/topo
-  log_begin_end steps/nnet3/chain/gen_topo.py ${gen_topo_begin_utc}
+    if [ -d data/lang_chain ]; then
+        if [ data/lang_chain/L.fst -nt data/lang/L.fst ]; then
+            echo "$0: data/lang_chain already exists, not overwriting it; continuing"
+        else
+            echo "$0: data/lang_chain already exists and seems to be older than data/lang..."
+            echo " ... not sure what to do.  Exiting."
+            exit 1;
+        fi
+    else
+        cp -r data/lang data/lang_chain
+        silphonelist=$(cat data/lang_chain/phones/silence.csl) || exit 1;
+        nonsilphonelist=$(cat data/lang_chain/phones/nonsilence.csl) || exit 1;
+        # Use our special topology... note that later on may have to tune this
+        # topology.
+        gen_topo_begin_utc=$(now_utc)
+        steps/nnet3/chain/gen_topo.py $nonsilphonelist $silphonelist \
+                                      > data/lang_chain/topo
+        log_begin_end steps/nnet3/chain/gen_topo.py ${gen_topo_begin_utc}
+    fi
 fi
 
-echo
-echo 'Get the alignments as lattices (gives the chain training more freedom).'
-echo
+gmm_dir=exp/$gmm
+ali_dir=exp/${gmm}_ali_${train_set}_sp_comb
+lang=data/lang_chain
+lores_train_data_dir=data/${train_set}_sp_comb
 
-align_fmllr_lats_begin_utc=$(now_utc)
-steps/align_fmllr_lats.sh --nj $nJobs --cmd "$train_cmd" ${lores_train_data_dir} \
-    data/lang $gmm_dir $lat_dir
-log_begin_end steps/align_fmllr_lats.sh ${align_fmllr_lats_begin_utc}
-rm $lat_dir/fsts.*.gz # save space
+if [ $stage -le 10 ]; then
+    echo
+    echo 'Get the alignments as lattices (gives the chain training more freedom).'
+    echo
 
-echo
-echo 'Build a tree using our new topology.  We know we have alignments for the'
-echo 'speed-perturbed data (local/nnet3/run_ivector_common.sh made them), so use'
-echo 'those.'
-echo
-
-if [ -f $tree_dir/final.mdl ]; then
-  echo "$0: $tree_dir/final.mdl already exists, refusing to overwrite it."
-  exit 1;
+    align_fmllr_lats_begin_utc=$(now_utc)
+    steps/align_fmllr_lats.sh --nj $nJobs --cmd "$train_cmd" \
+                              ${lores_train_data_dir} \
+                              data/lang \
+                              $gmm_dir \
+                              $lat_dir
+    log_begin_end steps/align_fmllr_lats.sh ${align_fmllr_lats_begin_utc}
+    rm $lat_dir/fsts.*.gz # save space
 fi
 
-build_tree_begin_utc=$(now_utc)
-steps/nnet3/chain/build_tree.sh --frame-subsampling-factor 3 \
-    --context-opts "--context-width=2 --central-position=1" \
-    --cmd "$train_cmd" 4000 ${lores_train_data_dir} data/lang_chain $ali_dir $tree_dir
-log_begin_end steps/nnet3/chain/build_tree.sh ${build_tree_begin_utc}
+if [ $stage -le 11 ]; then
+    echo
+    echo 'Build a tree using our new topology.  We know we have alignments for the'
+    echo 'speed-perturbed data (local/nnet3/run_ivector_common.sh made them), so use'
+    echo 'those.'
+    echo
+
+    if [ -f $tree_dir/final.mdl ]; then
+        echo "$0: $tree_dir/final.mdl already exists, refusing to overwrite it."
+        exit 1;
+    fi
+
+    build_tree_begin_utc=$(now_utc)
+    steps/nnet3/chain/build_tree.sh \
+        --frame-subsampling-factor 3 \
+        --context-opts "--context-width=2 --central-position=1" \
+        --cmd "$train_cmd" \
+        4000 ${lores_train_data_dir} data/lang_chain $ali_dir $tree_dir
+    log_begin_end steps/nnet3/chain/build_tree.sh ${build_tree_begin_utc}
+fi
